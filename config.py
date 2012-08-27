904f9bb033c07cd125f4ce7d0ecdd23b533e7661
Siddhartha Sahu <sh.siddhartha@gmail.com>
2012-08-27 23:36:23 +0530

    new version 2.0

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
-EMAIL = ''
-PASSWORD = ''
-dwdlist=['mp4']
+email = ''
+password = ''
+downloadlist=['mp4']
+foldermapping={'crypto-2012-003':'crypto','algs4partI-2012-001':'algs4partI','qcomp-2012-001':'qcomp','bigdata-2012-001':'bigdata'}
+downloadpath=''
