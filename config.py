20c5866620216b6b4defc106e5c1e93d4336e4e2
Siddhartha Sahu <sh.siddhartha@gmail.com>
2012-09-30 14:24:14 +0530

    added better folder names for courses

--- config.py
+++ config.py
@@ -1,5 +1,21 @@
 email = ''
 password = ''
 downloadlist=['mp4']
-foldermapping={'crypto-2012-003':'crypto','algs4partI-2012-001':'algs4partI','qcomp-2012-001':'qcomp','bigdata-2012-001':'bigdata'}
+foldermapping = {
+'algs4partI-2012-001'           :   'algorithms-partI', 
+'bigdata-2012-001'              :   'web-intelligence-and-big-data',
+'comparch-2012-001'             :   'computer-architecture',
+'compdata-2012-001'             :   'computing-for-data-analysis',
+'crypto-2012-003'               :   'cryptography-part1',
+'friendsmoneybytes-2012-001'    :   'networks-friendsmoneybytes', 
+'ml-2012-002'                   :   'machine-learning',
+'networks-2012-001'             :   'networked-life', 
+'pgm-2012-002'                  :   'probabilistic-graphical-models',
+'progfun-2012-001'              :   'functional-programming-principles-in-sala', 
+'programming1-2012-001'         :   'learn-to-program-the-fundamentals', 
+'qcomp-2012-001'                :   'quantum-mechanics-and-quantum-computing',
+'scientificcomp-2012-001'       :   'scientific-computing',
+'sna-2012-001'                  :   'social-network-analysis', 
+'stats1-2012-001'               :   'statistics-one'
+}
 downloadpath=''
