bcf519713c9b52602026dbd28913cb0bdf29a71e
Siddhartha Sahu <sh.siddhartha@gmail.com>
2012-12-14 19:32:37 +0530

    updated config file folder mappings

--- config.py
+++ config.py
@@ -1,21 +1,26 @@
 email = ''
 password = ''
-downloadlist=['mp4']
+downloadlist = ['pdf', 'mp4', 'srt']
 foldermapping = {
-'algs4partI-2012-001'           :   'algorithms-partI', 
+'algs4partI-2012-001'           :   'algorithms-partI',
+'algo'                          :   'algorithms-design-and-analysis-part1',
+'algo2-2012-001'                :   'algorithms-design-and-analysis-part2',
 'bigdata-2012-001'              :   'web-intelligence-and-big-data',
 'comparch-2012-001'             :   'computer-architecture',
 'compdata-2012-001'             :   'computing-for-data-analysis',
 'crypto-2012-003'               :   'cryptography-part1',
-'friendsmoneybytes-2012-001'    :   'networks-friendsmoneybytes', 
+'db'                            :   'introduction-to-databases',
+'friendsmoneybytes-2012-001'    :   'networks-friendsmoneybytes',
+'interactivepython-2012-001'    :   'interactive-programming-in-python',
 'ml-2012-002'                   :   'machine-learning',
-'networks-2012-001'             :   'networked-life', 
+'networks-2012-001'             :   'networked-life',
+'neuralnets-2012-001'           :   'neural-networks-for-machine-learning',
 'pgm-2012-002'                  :   'probabilistic-graphical-models',
-'progfun-2012-001'              :   'functional-programming-principles-in-sala', 
-'programming1-2012-001'         :   'learn-to-program-the-fundamentals', 
+'progfun-2012-001'              :   'functional-programming-principles-in-scala',
+'programming1-2012-001'         :   'learn-to-program-the-fundamentals',
 'qcomp-2012-001'                :   'quantum-mechanics-and-quantum-computing',
 'scientificcomp-2012-001'       :   'scientific-computing',
-'sna-2012-001'                  :   'social-network-analysis', 
+'sna-2012-001'                  :   'social-network-analysis',
 'stats1-2012-001'               :   'statistics-one'
 }
-downloadpath=''
+downloadpath = '.'
